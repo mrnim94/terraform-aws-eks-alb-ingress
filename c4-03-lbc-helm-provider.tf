399a6ccb2fe23f41fa3c9e951c23872df2a8a0a6
mr nim <mrnim@nimtechnology.com>
2022-11-28 00:54:18 +0700

    fix error eks_cluster_id

--- c4-03-lbc-helm-provider.tf
+++ c4-03-lbc-helm-provider.tf
@@ -6,7 +6,7 @@ data "aws_eks_cluster_auth" "cluster" {
 # HELM Provider
 provider "helm" {
   kubernetes {
-    host                   = var.cluster_endpoint
+    host                   = var.eks_cluster_id
     cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
